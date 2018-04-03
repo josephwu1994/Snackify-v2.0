48f5f4e8a0441d7332306e18c3b8afd37e3281f4
josephwu1994 <josephwu1994@gmail.com>
2018-04-02 23:23:41 -0700

    d

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -4,79 +4,79 @@ import shortid from 'shortid';
 import ThumbsUp from 'react-icons/lib/fa/thumbs-up';
 
 class Entry extends Component {
-    constructor(props){
+	constructor(props) {
 		super(props)
 		this.deletePost = this.deletePost.bind(this);
-    this.voteUp = this.voteUp.bind(this);
+		this.voteUp = this.voteUp.bind(this);
 	}
 
-  voteUp() {
-    fetch('/voteup', {
+	voteUp() {
+		fetch('/voteup', {
 			method: 'POST',
-			credentials: 'same-origin',
 			headers: {
+				credentials: 'same-origin',
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-        username: this.props.username,
-        postby: this.props.userPost.postby,
+				username: this.props.username,
+				postby: this.props.userPost.postby,
 			})
-      .then(res => res.json())
-      .then(json => alert(json))
-    });
-  }
+		})
+			.then(res => location.reload())
+	};
 
-    deletePost(id, username) {
-        fetch('/delete', {
-            method: 'POST',
-            headers: {
-                credentials: "same-origin",
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({id: id, username: username})
-          }).then(result => {
-              location.reload();
-        }).catch(err => {
-            console.log('ERROR!', err);
-          });
-    }
 
-    render() {
+	deletePost(id, username) {
+		fetch('/delete', {
+			method: 'POST',
+			headers: {
+				credentials: "same-origin",
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ id: id, username: username })
+		}).then(result => {
+			location.reload();
+		}).catch(err => {
+			console.log('ERROR!', err);
+		});
+	}
 
-      let id;
-			let username;
-			let commentDisplay = [<div className="None"></div>];
-      if (this.props.usernameLoggedIn === this.props.userPost.postby) {
-        if(this.props.userPost.comments !== undefined) {
-           commentDisplay = this.props.userPost.comments.map( (comment, i) => {
-            return <Comment key={(i+shortid.generate()).toString()} commentInfo ={comment} />
-          })
-        }
+	render() {
 
-        return (
-            <div className='entry'>
-              <div>{this.props.userPost.posby}</div>
-              <img className='entryImg' src={this.props.userPost.snacklink} />
-              {commentDisplay}
-              <div className="votes">{this.props.userPost.votes}</div>
-              <button className="thumbsBtn" onClick={this.voteUp}><ThumbsUp className="thumbs" /></button>
-							<button className="deleteBtn" onClick={()=>{this.deletePost(this.props.userPost.id, this.props.usernameLoggedIn)}} > Delete </button>
-            </div>
-        );
-    } else {
-        return (
-            <div className='entry'>
-              <div>{this.props.userPost.posby}</div>
-              <img className='entryImg' src={this.props.userPost.snackPhoto} />
-              {commentDisplay}
-              <div className="votes">{this.props.userPost.votes}</div>
-              <button className="thumbsBtn" onClick={this.voteUp}><ThumbsUp className="thumbs" /></button>
-            </div>
-        )
-      }
-    }
+		let id;
+		let username;
+		let commentDisplay = [<div className="None"></div>];
+		if (this.props.usernameLoggedIn === this.props.userPost.postby) {
+			if (this.props.userPost.comments !== undefined) {
+				commentDisplay = this.props.userPost.comments.map((comment, i) => {
+					return <Comment key={(i + shortid.generate()).toString()} commentInfo={comment} />
+				})
+			}
 
-    
+			return (
+				<div className='entry'>
+					<div>{this.props.userPost.posby}</div>
+					<img className='entryImg' src={this.props.userPost.snacklink} />
+					{commentDisplay}
+					<div className="votes">{this.props.userPost.votes}</div>
+					<button className="thumbsBtn" onClick={this.voteUp}><ThumbsUp className="thumbs" /></button>
+					<button className="deleteBtn" onClick={() => { this.deletePost(this.props.userPost.id, this.props.usernameLoggedIn) }} > Delete </button>
+				</div>
+			);
+		} else {
+			return (
+				<div className='entry'>
+					<div>{this.props.userPost.posby}</div>
+					<img className='entryImg' src={this.props.userPost.snackPhoto} />
+					{commentDisplay}
+					<div className="votes">{this.props.userPost.votes}</div>
+					<button className="thumbsBtn" onClick={this.voteUp}><ThumbsUp className="thumbs" /></button>
+				</div>
+			)
+		}
+	}
+
+
 }
 
 export default Entry;
